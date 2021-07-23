a367e30b1e11cda903d02680d8e42f6e46901210
20045225 <20045225@DESKTOP-NJGC59J>
2021-07-23 13:49:43 +0800

    Edited: addChromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -188,7 +188,8 @@ public class ResourceCentre {
 	}	
 	public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
 		// write your code here
-
+		chromebookList.add(cb);
+		System.out.println("Chromebook added!");
 	}
 	
 	//================================= Option 3 Loan an item (CRUD - Update) =================================
