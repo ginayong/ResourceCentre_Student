9064df7c517a67301f3a56e6a60c240450c70ec5
20006896 <20006896@DESKTOP-0BUNQF0>
2021-07-23 14:05:50 +0800

    edited doReturnChromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -283,6 +283,14 @@ public class ResourceCentre {
 	public static boolean doReturnChromebook(ArrayList<Chromebook> chromebookList,String tag){
 		boolean isReturned = false;
 		// write your code here
+		
+		for (int i = 0; i < chromebookList.size(); i++) {
+			if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag()) && chromebookList.get(i).getIsAvailable() == false) {
+				chromebookList.get(i).setIsAvailable(true);
+				chromebookList.get(i).setDueDate("");
+				isReturned = true;
+			}
+		}
 		return isReturned;
 	}
 	public static void returnChromebook(ArrayList<Chromebook> chromebookList) {
