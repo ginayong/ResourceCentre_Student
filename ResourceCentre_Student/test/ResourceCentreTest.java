70bbf0d8b65dd63f12f895443e3543846ce56833
20039789 <20039789@DESKTOP-F898QT2.lan>
2021-07-23 14:47:28 +0800

    Updated:testRetrieveAllChromebook()

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -107,8 +107,8 @@ public class ResourceCentreTest {
 		
 		allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
 		
-		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0011", "My Google Chromebook 1st", "Yes", "Mac OS");
-		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0012", "SAMSUNG Chromebook 4+", "Yes", "Win 10");
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0011", "My Google Chromebook 1st", "Yes", "", "Mac OS");
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0012", "SAMSUNG Chromebook 4+", "Yes", "", "Win 10");
 		
 		assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
 	}
